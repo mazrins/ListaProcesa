2b764964d32e0d19f127e3e5aa5fc3ec1f58f78f
Marijan Zrins <MaZrins@outlook.com>
2019-02-14 17:57:54 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace ListaProcesa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ImeProcesa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProcessID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Priority = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MemoryUsage = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProcessorTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ImeProcesa,
+            this.ProcessID,
+            this.Priority,
+            this.MemoryUsage,
+            this.ProcessorTime});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(800, 450);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(800, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Zatvori";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ImeProcesa
+            // 
+            this.ImeProcesa.HeaderText = "Ime Procesa";
+            this.ImeProcesa.Name = "ImeProcesa";
+            this.ImeProcesa.ReadOnly = true;
+            // 
+            // ProcessID
+            // 
+            this.ProcessID.HeaderText = "Proces ID";
+            this.ProcessID.Name = "ProcessID";
+            this.ProcessID.ReadOnly = true;
+            // 
+            // Priority
+            // 
+            this.Priority.HeaderText = "Prioritet";
+            this.Priority.Name = "Priority";
+            this.Priority.ReadOnly = true;
+            // 
+            // MemoryUsage
+            // 
+            this.MemoryUsage.HeaderText = "Zauzeće memorije";
+            this.MemoryUsage.Name = "MemoryUsage";
+            this.MemoryUsage.ReadOnly = true;
+            // 
+            // ProcessorTime
+            // 
+            this.ProcessorTime.HeaderText = "Procesorsko Vrijeme";
+            this.ProcessorTime.Name = "ProcessorTime";
+            this.ProcessorTime.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Lista Procesa";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ImeProcesa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProcessID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Priority;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MemoryUsage;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProcessorTime;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ListaProcesa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listAllProcesses();
+            timer1.Start();
+            
+
+        }
+        public string processMethod(string processName)
+        {
+            PerformanceCounter myAppCpu = new PerformanceCounter("Process","% Processor Time", processName, true);
+            double ptc = myAppCpu.NextValue();
+            Thread.Sleep(50);
+            ptc = myAppCpu.NextValue();
+            return ptc.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        
+        public void listAllProcesses()
+        {
+          
+            Process[] AllProcess = Process.GetProcesses();
+            foreach (Process proc in AllProcess)
+            {
+                var a = processMethod(proc.ProcessName.ToString());
+                try
+                {
+                    dataGridView1.Rows.Add(proc.ProcessName, proc.Id,proc.BasePriority,proc.PrivateMemorySize64,a);
+
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    throw;
+                }
+            }
+            
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            dataGridView1.Update();
+            dataGridView1.Refresh();
+            
+
+        }
+    }
+}
